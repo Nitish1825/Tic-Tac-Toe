5aa764cfc3fa37ef1a79250c8ae159d52a19d35c
NagaNitishReddy <naganitishreddy@gmail.com>
2025-03-01 08:51:26 +0530

    made app component leaner.Project Done !

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,13 @@ import GameBoard from "./Components/GameBoard"
 import Log from "./Components/Log";
 import { WINNING_COMBINATIONS } from "./Components/winning-combinations";
 import GameOver from "./Components/GameOver";
-const initialGameBoard=[[null,null,null],[null,null,null],[null,null,null]];
+
+const PLAYERS={
+  X : 'PLAYER 1',
+  O : 'PLAYER 2'
+}
+
+const INITIAL_GAME_BOARD=[[null,null,null],[null,null,null],[null,null,null]];
 
 function deriveActivePlayer(gameTurns){
   let CurrentPlayer='X'
@@ -13,17 +19,40 @@ function deriveActivePlayer(gameTurns){
       }
       return CurrentPlayer;
 }
-function App() {
-  /*we can get activeplayer from gameturns so we are removing activeplayer
-  const[activeplayer,setActivePlayer]=useState('X');
-  */
-  const[gameTurns,setGameTurns]=useState([]);
-  const[players,setPlayers]=useState(
-    {
-      'X':'Player 1',
-      'O':'Player 2'
+
+function derviveWinner(gameBoard, players){
+  let winner;
+
+  for(const combination of WINNING_COMBINATIONS){
+    const firstSquareSymbol =gameBoard[combination[0].row][combination[0].column];
+    const secondSquareSymbol =gameBoard[combination[1].row][combination[1].column];
+    const thirdSquareSymbol =gameBoard[combination[2].row][combination[2].column];
+    if(
+      firstSquareSymbol && firstSquareSymbol === secondSquareSymbol && firstSquareSymbol === thirdSquareSymbol
+    ){
+      winner = players[firstSquareSymbol];
     }
-  );
+    }
+
+    return winner;
+  }
+
+
+  function deriveGameBoard(gameTurns){
+    let gameBoard=[...INITIAL_GAME_BOARD.map(array=>[...array])];
+
+    for(const turn of gameTurns){
+        const{ square, player }=turn;
+        const{row,col}=square;
+        
+        gameBoard[row][col]=player;
+    }
+    return gameBoard;
+  }
+
+function App() {
+  const[gameTurns,setGameTurns]=useState([]);
+  const[players,setPlayers]=useState(PLAYERS);
   function handlePlayerNameChange(symbol,newName){
     setPlayers((prevPlayers)=>{
       return{
@@ -35,27 +64,9 @@ function App() {
 
   const activePlayer=deriveActivePlayer(gameTurns);
 
-  let gameBoard=[...initialGameBoard.map(array=>[...array])];
+  const gameBoard=deriveGameBoard(gameTurns);
 
-    for(const turn of gameTurns){
-        const{ square, player }=turn;
-        const{row,col}=square;
-        
-        gameBoard[row][col]=player;
-    }
-
-    let winner;
-
-  for(const combination of WINNING_COMBINATIONS){
-    const firstSquareSymbol =gameBoard[combination[0].row][combination[0].column];
-    const secondSquareSymbol =gameBoard[combination[1].row][combination[1].column];
-    const thirdSquareSymbol =gameBoard[combination[2].row][combination[2].column];
-    if(
-      firstSquareSymbol && firstSquareSymbol === secondSquareSymbol && firstSquareSymbol === thirdSquareSymbol
-    ){
-      winner = players[firstSquareSymbol];
-    }
-  }
+   const winner=derviveWinner(gameBoard,players)
 
   const hasDraw = gameTurns.length === 9 && !winner;
 
@@ -77,8 +88,8 @@ function App() {
     <main>
       <div id="game-container"> 
       <ol id="players" className="highlight-player">
-          <Player InitialName="Player1" Symbol="X" isActive={activePlayer ==='X'} onChangeName={handlePlayerNameChange}/>
-          <Player InitialName="Player2" Symbol="O" isActive={activePlayer ==='O'}/>
+          <Player InitialName={PLAYERS.X} Symbol="X" isActive={activePlayer ==='X'} onChangeName={handlePlayerNameChange}/>
+          <Player InitialName={PLAYERS.O} Symbol="O" isActive={activePlayer ==='O'}/>
       </ol>
       {(winner|| hasDraw) && <GameOver winner={winner} onRestart={handleRestart}/>}
      <GameBoard onSelectSquare={handleSelectSquare} board={gameBoard}/>
--- src/Components/Log.jsx
+++ src/Components/Log.jsx
@@ -2,7 +2,7 @@ export default function Log({turns}){
     return(
         <ol id="log">
             {turns.map((turn)=> (
-                <li key={`${turn.square.row}${turn.square.col}`}>{turn.player} Selected {turn.square.row}{turn.square.col}</li>
+                <li key={`${turn.square.row}${turn.square.col}`}>{turn.player} Selected {turn.square.row},{turn.square.col}</li>
             ))}
             
         </ol>
